7dfaefe3205ac71ca3dd37189a5f8fe81a2c190d
Rameez-Ahmed <rjalbani32@gmail.com>
2020-08-03 22:20:44 +0500

    Update Rplayer.py

--- Rplayer.py
+++ Rplayer.py
@@ -13,19 +13,11 @@ playlist_box.pack(pady=20)
 control_frame = Frame(root)
 control_frame.pack(pady=20)
 
-# Creating play/stop etc buttons 
-back_button = button(control_frame, text="Back")
-forward_button = button(control_frame, text="Forward")
-play_button = button(control_frame, text="Play")
-pause_button = button(control_frame, text="Pause")
-stop_button = button(control_frame, text="Stop")
-
-back_button.grid(row=0, column=0)
-forward_button.grid(row=0, column=1)
-play_button.grid(row=0, column=2)
-pause_button.grid(row=0, column=3)
-stop_button.grid(row=0, column=4)
-
-
+# Creating play/stop etc buttons
+back_button=Button(control_frame, text="Back")
+forward_button=Button(control_frame, text="Forward")
+play_button=Button(control_frame, text="Play")
+pause_button=Button(control_frame, text="Pause")
+stop_button=Button(control_frame, text="Stop")
 
 root.mainloop()
