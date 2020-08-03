bb67d5366d82c74dc23be28c224d2feed6bf30d9
Rameez-Ahmed <rjalbani32@gmail.com>
2020-08-03 22:01:06 +0500

    Update Rplayer.py
    Button coding

--- Rplayer.py
+++ Rplayer.py
@@ -9,8 +9,22 @@ root.geometry("500x400")
 playlist_box = Listbox(root, bg="black",fg="green", width=60)
 playlist_box.pack(pady=20)
 
+# Creating Button frame
+control_frame = Frame(root)
+control_frame.pack(pady=20)
 
+# Creating play/stop etc buttons 
+back_button = button(control_frame, text="Back")
+forward_button = button(control_frame, text="Forward")
+play_button = button(control_frame, text="Play")
+pause_button = button(control_frame, text="Pause")
+stop_button = button(control_frame, text="Stop")
 
+back_button.grid(row=0, column=0)
+forward_button.grid(row=0, column=1)
+play_button.grid(row=0, column=2)
+pause_button.grid(row=0, column=3)
+stop_button.grid(row=0, column=4)
 
 
 
