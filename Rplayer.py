94f9fcececacf36c2e8d281a3d31b6f03e6635a0
Rameez-Ahmed <rjalbani32@gmail.com>
2020-08-03 23:00:39 +0500

    Update Rplayer.py

--- Rplayer.py
+++ Rplayer.py
@@ -4,26 +4,33 @@ root = Tk()
 # tiltle name
 root.title("Rj mp3 Player")
 # size of player
-root.geometry("500x400")
+root.geometry("700x500")
 # playlist box
-playlist_box = Listbox(root, bg="black",fg="green", width=60)
+playlist_box = Listbox(root, bg="black",fg="green", width=80, height=12)
 playlist_box.pack(pady=20)
+
+#Adding images to buttons
+back_btn_img=PhotoImage(file='images/back.png')
+forward_btn_img=PhotoImage(file='images/forward.png')
+play_btn_img=PhotoImage(file='images/play.png')
+pause_btn_img=PhotoImage(file='images/pause.png')
+stop_btn_img=PhotoImage(file='images/stop.png')
 
 # Creating Button frame
 control_frame = Frame(root)
 control_frame.pack(pady=20)
 
 # Creating play/stop etc buttons
-back_button=Button(control_frame, text="Back")
-forward_button=Button(control_frame, text="Forward")
-play_button=Button(control_frame, text="Play")
-pause_button=Button(control_frame, text="Pause")
-stop_button=Button(control_frame, text="Stop")
+back_button=Button(control_frame, image=back_btn_img)
+forward_button=Button(control_frame,image=forward_btn_img)
+play_button=Button(control_frame, image=play_btn_img)
+pause_button=Button(control_frame, image=pause_btn_img)
+stop_button=Button(control_frame, image=stop_btn_img)
 
-back_button.grid(row=0, column=0)
-forward_button.grid(row=0, column=1)
-play_button.grid(row=0, column=2)
-pause_button.grid(row=0, column=3)
-stop_button.grid(row=0, column=4)
+back_button.grid(row=0, column=0, padx=10)
+forward_button.grid(row=0, column=1, padx=10)
+play_button.grid(row=0, column=2, padx=10)
+pause_button.grid(row=0, column=3, padx=10)
+stop_button.grid(row=0, column=4, padx=10)
 
 root.mainloop()
