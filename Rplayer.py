87168271c03efba22e5432306d5db2cf5277226d
Rameez-Ahmed <rjalbani32@gmail.com>
2020-10-27 16:18:08 +0500

    update Rplayer.py

--- Rplayer.py
+++ Rplayer.py
@@ -1,10 +1,17 @@
 from tkinter import *
+from tkinter import filedialog
 
 root = Tk()
 # tiltle name
 root.title("Rj mp3 Player")
 # size of player
 root.geometry("700x500")
+##create function to playlist to add one songs
+def add_song():
+	song = filedialog.askopenfilename()
+#create function to playlist to add many songs
+def add_many_songs():
+	pass
 # playlist box
 playlist_box = Listbox(root, bg="black",fg="green", width=80, height=12)
 playlist_box.pack(pady=20)
@@ -32,5 +39,18 @@ forward_button.grid(row=0, column=1, padx=10)
 play_button.grid(row=0, column=2, padx=10)
 pause_button.grid(row=0, column=3, padx=10)
 stop_button.grid(row=0, column=4, padx=10)
+
+#Create Menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+#create Add song menu dropdowns
+add_song_menu = Menu(my_menu, tearoff=0)
+my_menu.add_cascade(label="Add Songs",menu=add_song_menu)
+#Add one song
+add_song_menu.add_command(label="Add One Song To Playlist", command=add_song)
+#Add many songs
+add_song_menu.add_command(label="Add Many Song To Playlist", command=add_many_songs)
+
 
 root.mainloop()
